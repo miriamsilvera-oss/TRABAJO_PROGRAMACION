30daf741a2ead26b05ab84292ff16ce68aec23d2
miriamsilvera-oss <miriam.silvera@estudiantes.utec.edu.uy>
2026-06-10 12:39:55 -0600

    comentario

---
+++ Funciones/divideinTwo.py
@@ -0,0 +1,2 @@
+# funcion dividir imagen en dos parte iguales Implementar una función que divida una imagen termográfica en dos mitades o cuadrantes iguales a la
+#  mitad de las columnas. La figura 1 muestra un ejemplo.
