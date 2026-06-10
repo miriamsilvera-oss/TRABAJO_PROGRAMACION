a129692532e8eadb6cd3a987af3df6ce38fcbbd2
miriamsilvera-oss <miriam.silvera@estudiantes.utec.edu.uy>
2026-06-10 12:56:50 -0600

    importar en main y dividir imagen en dos

--- Funciones/divideinTwo.py
+++ Funciones/divideinTwo.py
@@ -1,2 +1,4 @@
 # funcion dividir imagen en dos parte iguales Implementar una función que divida una imagen termográfica en dos mitades o cuadrantes iguales a la
 #  mitad de las columnas. La figura 1 muestra un ejemplo.
+def divideinTwo(imagen):
+    
---
+++ main.py
@@ -0,0 +1,17 @@
+from Funciones import cargarImagen
+from Funciones import divideinFour
+from Funciones import divideinTwo
+from Funciones import frequencyCounter
+from Funciones import getMinvalue
+from Funciones import getMaxValue
+from Funciones import getDiffTemp
+from Funciones import getMean
+from Funciones import getMedian
+from Funciones import getStd
+from Funciones import getVar
+from Funciones import plotImage
+from Funciones import plotLine
+from Funciones import plotSeveralimages
+
+
+
