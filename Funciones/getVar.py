58e81e5689451f306e7a338bf20ca88fcec12ec8
miriamsilvera-oss <miriam.silvera@estudiantes.utec.edu.uy>
2026-06-13 17:57:28 -0600

    cambios

--- Funciones/getVar.py
+++ Funciones/getVar.py
@@ -1,5 +1,12 @@
 #andrea
 # función que obtiene la varianza de temperaturas por encima de un umbral
+from getMean import getMean 
+
+"""La varianza mide en este caso cuanto se alejan las temperaturas de la media
+para calcularla necesitamos conocer:
+* las temperaturas
+* la media de las temperaturas
+* la cantidad de temperaturas """
 
 imagen = [[28.5, 31.2, 29.8, 32.1],
           [34.5, 33.7, 31.8, 30.2],
@@ -15,30 +22,32 @@ def getVar(imagen, temperatura_umbral):
         temperatura_umbral (float): Temperatura mínima a considerar.
     Retorna:
         float: Varianza de las temperaturas encontradas.
+        Se reutiliza la funcion para calcular la media
     """
-    # paso 1: juntamos todas las temperaturas que superan el umbral
-    valores = []
-    for fila in imagen:                          # recorremos cada fila
-        for temperatura in fila:                 # recorremos cada temperatura
-            if temperatura > temperatura_umbral: # solo tomamos las que superan el umbral
-                valores.append(temperatura)      # la agregamos a la lista
 
-    # paso 2: calculamos la media sin usar sum()
-    sumatoria = 0
-    for v in valores:                # recorremos cada valor
-        sumatoria = sumatoria + v    # sumamos cada temperatura
-    media = sumatoria / len(valores) # dividimos por la cantidad de valores
+    suma_cuadrados = 0
+    contador = 0
 
-    # paso 3: calculamos la varianza
-    sumatoria_dif = 0                                    # empezamos en 0
-    for v in valores:                                    # recorremos cada valor
-        diferencia = v - media                           # calculamos la diferencia con la media
-        sumatoria_dif = sumatoria_dif + diferencia ** 2  # la elevamos al cuadrado y sumamos
-    varianza = sumatoria_dif / len(valores)              # dividimos por la cantidad de valores
+    media = getMean(imagen, temperatura_umbral)
+    # como media tiene una cadena de texto en su interior para CASO DEPURACION
+    if isinstance(media,str):
+        return media
+# esto garantiza que si getMean ingresa al caso depuracion, el programa no ingresa al bucle y manda mensaje error 
+# si getMean es un numero entonces ingresa al bucle for a continuacion 
+    for fila in imagen:
+        for temperatura in fila:
+            if temperatura > temperatura_umbral:
+                suma_cuadrados += (temperatura - media)**2
+                contador += 1
+    if contador == 0:
+        return "Ninguna temperatura supero el umbral establecido, favor de cambiar el umbral "
+    
+    varianza = suma_cuadrados// contador
 
-    return varianza                  # retornamos la varianza
-
+    return varianza
+    
 #Prueba de función 
 if __name__ == "__main__":
     resultado = getVar(imagen, temperatura_umbral)
     print(f"Varianza: {resultado}")
+
